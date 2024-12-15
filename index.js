2c4d18ca22eb17923e4e9ad542bafdcca5d6a282
Yashi Gupta <145125817+yashiigupta@users.noreply.github.com>
2024-12-15 12:17:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const getTodosSync = () => {};
+
+const getTodoSync = (id) => {};
+
+const createTodoSync = (todo) => {};
+
+const updateTodoSync = (id, updates) => {};
+
+const deleteTodoSync = (id) => {};
+
+module.exports = {
+  getTodosSync,
+  getTodoSync,
+  createTodoSync,
+  updateTodoSync,
+  deleteTodoSync,
+};
